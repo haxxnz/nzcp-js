3a0bb364e6f428dc952b2ea4f002b5c8eb30c5a7
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-11-09 02:10:17 +1300

    don't attach to window

--- src/browser.ts
+++ src/browser.ts
@@ -3,8 +3,4 @@
  * The code executes when loaded in a browser.
  */
 import { verifyPass } from './main'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).verifyPass = verifyPass  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
-
-console.log('Method "verifyPass" was added to the window object. You can try it yourself by just entering "await verifyPass()"')
+export { verifyPass }
