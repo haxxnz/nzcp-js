7cfcf2b00d793a85d501fb27a4404c8089980c19
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-11-08 22:57:45 +1300

    export verifyPass

--- src/browser.ts
+++ src/browser.ts
@@ -2,9 +2,9 @@
  * This file is the entrypoint of browser builds.
  * The code executes when loaded in a browser.
  */
-import { foo } from './main'
+import { verifyPass } from './main'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).foo = foo  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
+(window as any).verifyPass = verifyPass  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
 
-console.log('Method "foo" was added to the window object. You can try it yourself by just entering "await foo()"')
+console.log('Method "verifyPass" was added to the window object. You can try it yourself by just entering "await verifyPass()"')
