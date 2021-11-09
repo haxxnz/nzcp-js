b586715adbade8d74200b56040f675a4e8bb0cf1
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-11-09 19:25:05 +1300

    add comments

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,10 @@
 
+// convert js timestamp to unix timestamp
 export function currentTimestamp(): number {
   return Date.now() / 1000;
 }
 
+// from https://nzcp.covid19.health.nz/#adding-base32-padding
 export function addBase32Padding(base32InputNoPadding: string): string {
   let result = base32InputNoPadding;
   while ((result.length % 8) !== 0) {
