bef8fa304d5676d33c076e8e19d692a0b2c2ecb6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-11-10 22:03:11 +1300

    revert encodeCanonical

--- src/crypto.ts
+++ src/crypto.ts
@@ -32,7 +32,7 @@ export function validateCOSESignature(
   //     payload : bstr
   // ]
   const SigStructure = ["Signature1", Buffer.from(protected_ as Buffer), Buffer.alloc(0), Buffer.from(payload_ as Buffer)];
-  const ToBeSigned = cbor.encodeCanonical(SigStructure);
+  const ToBeSigned = cbor.encode(SigStructure);
   const messageHash = sha256.digest(ToBeSigned);
   const signature = {
     r: signature_.slice(0, signature_.length / 2),
