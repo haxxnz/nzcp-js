26ae0fb874e0390cb1fed336e2b8bf4790922d2f
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-11-13 12:43:01 +1300

    lint

--- build.ts
+++ build.ts
@@ -1,29 +1,29 @@
-import { build } from "esbuild"
-import { nodeBuiltIns } from "esbuild-node-builtins"
+import { build } from "esbuild";
+import { nodeBuiltIns } from "esbuild-node-builtins";
 async function main() {
   const resultBrowser = await build({
     plugins: [nodeBuiltIns()],
-    entryPoints: ['src/browser.ts'],
+    entryPoints: ["src/browser.ts"],
     bundle: true,
-    outfile: 'dist/esbuild/browser.js',
+    outfile: "dist/esbuild/browser.js",
     sourcemap: "both",
     minify: true,
     platform: "browser",
     format: "cjs",
-    target: "es6"
-  })
-  console.log('resultBrowser', resultBrowser)
+    target: "es6",
+  });
+  console.log("resultBrowser", resultBrowser);
 
   const resultNode = await build({
-    entryPoints: ['src/node.ts'],
+    entryPoints: ["src/node.ts"],
     bundle: true,
-    outfile: 'dist/esbuild/node.js',
+    outfile: "dist/esbuild/node.js",
     sourcemap: "both",
     minify: true,
     platform: "node",
     format: "cjs",
-    target: "es6"
-  })
-  console.log('resultNode', resultNode)
+    target: "es6",
+  });
+  console.log("resultNode", resultNode);
 }
-main()
+main();
