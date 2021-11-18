a053e04fb3220566d529a9d72a8ed8bd2c0dd337
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-11-19 02:58:25 +1300

    export more stuff

--- src/browser.ts
+++ src/browser.ts
@@ -2,5 +2,5 @@
  * This file is the entrypoint of browser builds.
  * The code executes when loaded in a browser.
  */
-import { verifyPassURI, verifyPassURIWithTrustedIssuers } from "./main";
-export { verifyPassURI, verifyPassURIWithTrustedIssuers };
+import { verifyPassURI, verifyPassURIOffline, DID_DOCUMENTS, TRUSTED_ISSUERS } from "./main";
+export { verifyPassURI, verifyPassURIOffline, DID_DOCUMENTS, TRUSTED_ISSUERS };
