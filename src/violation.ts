81b4d53f3164d3decf161922dad572b83b00c1bc
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-11-23 23:30:33 +1300

    refactor Violation a bit - remove wrapping object

--- src/violation.ts
+++ src/violation.ts
@@ -1,13 +1,11 @@
 import { Violates } from "./generalTypes";
 
-interface ViolationOptions {
-  violates: Violates
-}
+type ViolationOptions = Violates;
 
-export class Violation extends Error{
+export class Violation extends Error {
   violates: Violates;
-  constructor(options: ViolationOptions){
-    super(options.violates.message);
-    this.violates = options.violates;
+  constructor(options: ViolationOptions) {
+    super(options.message);
+    this.violates = options;
   }
 }
