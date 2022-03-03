fa46a7efcb52eb65ac2eb66ab5f175cf00be7f59
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-04 00:25:53 +1300

    switch decodeCtiToJti to uint8array

--- src/jtiCti.test.ts
+++ src/jtiCti.test.ts
@@ -1,4 +1,3 @@
-import { Buffer } from "buffer";
 import { decodeCtiToJti } from "./jtiCti";
 import { Violation } from "./violation";
 
@@ -8,13 +7,13 @@ import { Violation } from "./violation";
 
 // Properly formed CWT Token ID with 16 octets
 test("properly formed cti is decode into jti", async () => {
-  const cti = Buffer.from("60a4f54d4e304332be33ad78b1eafa4b", "hex");
+  const cti = new Uint8Array([0x60, 0xa4, 0xf5, 0x4d, 0x4e, 0x30, 0x43, 0x32, 0xbe, 0x33, 0xad, 0x78, 0xb1, 0xea, 0xfa, 0x4b])
   const result = decodeCtiToJti(cti);
   expect(result).toBe("urn:uuid:60a4f54d-4e30-4332-be33-ad78b1eafa4b");
 });
 
 // Malformed CWT Token ID with 8 octets
 test("malformed cti returns unsuccessful result", async () => {
-  const cti = Buffer.from("60a4f54d4e304332", "hex");
+  const cti = new Uint8Array([0x60, 0xa4, 0xf5, 0x4d, 0x4e, 0x30, 0x43, 0x32])
   expect(() => decodeCtiToJti(cti)).toThrowError(Violation);
 });
